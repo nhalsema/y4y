687bf7e5bdc016b538f8301e46ee7111526a8acd
Nicolette Halsema <nicolettebh@gmail.com>
2023-12-22 21:11:00 -0800

    flying toasters background circa 1990. loving it.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.sass';
 import Timer from './components/Timer';
-import NavBar from './components/NavBar';
+// import NavBar from './components/NavBar';
+import FlyingToasters from './components/FlyingToasters';
 
 function App() {
   return (
     <div className="App">
+      <FlyingToasters />
       <h1>hello world</h1>
       <Timer
           initMinute={0}
           initSeconds={30}
       /> 
  
-      <NavBar />
+
     </div>
 
   );
